e76b4ff78625600151a80dd98a7ab1a8ea4ce957
Melina <83986218+Melinacant@users.noreply.github.com>
2021-10-04 23:46:52 -0300

    React App

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+const ButtonExampleShorthand = ({button_string, onClick}) => 
+<Button content={button_string} onClick={onClick} />
+
+
+  
+export default ButtonExampleShorthand

fc6de08ba42b4c0614ae1b5dd562fb7ecfef7439
Melina <83986218+Melinacant@users.noreply.github.com>
2021-10-04 23:42:48 -0300

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,18 @@
+import ButtonExampleShorthand from '../Button/Button';
 import './Card.css';
 
-const Card = ({ image, name }) => { 
+
+const Card = ({ image, name }) => {
+  const handleOnClick = () => {
+    alert('Ver más')
+  }
   return (
     <div 
       className="card-container"
       style={{ backgroundImage: `url(${image})` }} >
+      <div className="card-button">
+        <ButtonExampleShorthand button_string='Ver más' onClick={handleOnClick} />
+      </div>
       <div className="card-content">
         <span className="card-span">{name}</span>
       </div>
